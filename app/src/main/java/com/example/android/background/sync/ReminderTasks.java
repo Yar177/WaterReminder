a97c65bdeaff43d1b6dcdb0160c418bd724d77f7
Yar <yarsher.dev@gmail.com>
2019-01-21 23:18:53 -0500

    notificationAction b init

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -17,6 +17,7 @@ package com.example.android.background.sync;
 
 import android.content.Context;
 
+import com.example.android.background.utilities.NotificationUtils;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class ReminderTasks {
@@ -30,6 +31,8 @@ public class ReminderTasks {
     public static void executeTask(Context context, String action) {
         if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
             incrementWaterCount(context);
+        }else if(ACTION_DISMISS_NOTIFICATION.equals(action)){
+            NotificationUtils.clearAllNotification(context);
         }
     }
 
