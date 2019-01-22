91eb63856598575e984e276fd62459ae881f6123
Yar <yarsher.dev@gmail.com>
2019-01-22 00:17:29 -0500

    jobDispatcher

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -25,6 +25,7 @@ public class ReminderTasks {
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
 
     public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
+    public static final String ACTION_CHARGING_REMINDER = "charging-reminder";
 
 
 
@@ -33,7 +34,15 @@ public class ReminderTasks {
             incrementWaterCount(context);
         }else if(ACTION_DISMISS_NOTIFICATION.equals(action)){
             NotificationUtils.clearAllNotification(context);
+        }else if(ACTION_CHARGING_REMINDER.equals(action)){
+            issueChargingReminder(context);
         }
+    }
+
+    private static void issueChargingReminder(Context context) {
+        PreferenceUtilities.incrementChargingReminderCount(context);
+        NotificationUtils.remindUserBecauseCharging(context);
+
     }
 
     private static void incrementWaterCount(Context context) {
--- app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -53,9 +53,9 @@ public class NotificationUtils {
 
 
         NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(context, WATER_REMINDER_NOTIFICATION_CHANEL_ID)
+                new NotificationCompat.Builder(context , WATER_REMINDER_NOTIFICATION_CHANEL_ID)
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
-                .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+                .setSmallIcon(R.drawable.ic_drink_notification)
                 .setLargeIcon(largeIcon(context))
                 .setContentTitle(context.getString(R.string.charging_reminder_notification_title ))
                 .setContentText(context.getString(R.string.charging_reminder_notification_body))
